c75d998d5730a87a69bc1cf4e77d45f5c58a3986
brian.maina@student.moringaschool.com <brianmaina2>
2023-02-07 16:41:56 +0300

    commit tree 4fb832a4b701ab920df3c36986b8da173387623b

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def happy_new_year
   # your code here
+  def happy_new_year
+    10.downto(1) do |i|
+      puts i
+    end
+    puts "Happy New Year!"
+  end
+  
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
